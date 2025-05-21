63caa3ce5d05dc58879f3d40d563963df724bfba
Eftakharalamnoor <eftakharalamnoor@gmail.com>
2025-05-21 18:50:09 +0600

    Hero Section Working

--- Frontend/src/app/(routes)/components/HeroSection.tsx
+++ Frontend/src/app/(routes)/components/HeroSection.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import React from "react";
 import HeroImage from "@/assets/HeroImage.png";
 import HeroImage2 from "@/assets/HeroImage-2.jpg";
+import HeroImage3 from "@/assets/HeroImage-3.jpg";
+import HeroImage4 from "@/assets/HeroImage-4.jpg";
 import { IoPhonePortraitOutline } from "react-icons/io5";
 import { FaHome } from "react-icons/fa";
+import { Worker } from "@/assets/svg/Worker";
+import { Map } from "@/assets/svg/Map";
 const HeroSection = () => {
   return (
     <div className="flex justify-end">
@@ -24,10 +28,16 @@ const HeroSection = () => {
         </div>
 
         <div className="w-[70%] relative flex justify-end">
-          <div className="absolute left-0 top-5 size-[100px] rounded-full bg-white flex justify-center items-center">
+          <div className="absolute left-[223px] top-[138px] z-20 size-[100px] rounded-full bg-white flex justify-center items-center">
             <FaHome color="#FF460D" size={50} />
           </div>
-          <div className="absolute top-0 left-0 w-[474px] flex justify-center items-center rounded-full h-[474px] bg-[#1D4ED8]">
+          <div className="absolute  bottom-[180px] left-[70px] z-20 size-[100px] rounded-full bg-white flex justify-center items-center">
+            <Worker className="size-[50px]" />
+          </div>
+          <div className="absolute bottom-[180px] left-[400px] z-20 size-[100px] rounded-full bg-white flex justify-center items-center">
+            <Map className="size-[50px]" />
+          </div>
+          <div className="absolute top-50 left-[51px] w-[474px] flex justify-center items-center rounded-full h-[474px] bg-[#1D4ED8]">
             <Image
               className="size-[398px] rounded-full"
               src={HeroImage2}
